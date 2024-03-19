35a9bc800247294271aac8a170cc0af11084593a
DemetrioPadre <demetriopadre@gmail.com>
2024-03-19 22:42:11 +0100

    add movies

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
@@ -11,6 +12,9 @@ class PageController extends Controller
      * **/
     public function index()
     {
+        $movies = Movie::all();
+        dd($movies);
+
         return view('home');
     }
 }
