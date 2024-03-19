f991be03ef209438650ea06fc8fee11c03b9d82b
DemetrioPadre <148373150+DemetrioPadre@users.noreply.github.com>
2024-03-19 18:08:58 +0100

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Pagina iniziale')
+
+@section('main-content')
+    <section>
+        <div class="container py-4">
+            <h1>LARAVEL IS THE BEST </h1>
+        </div>
+    </section>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="bg-dark text-white">
+    <div class="container py-4 ">
+        &copy; {{ env('APP_AUTHOR', 'Demetrio Padre') }} {{ now()->year }}
+    </div>
+
+</footer>
