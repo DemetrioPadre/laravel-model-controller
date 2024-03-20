61125095260a1a1b09614e11df66326edd322bc8
DemetrioPadre <demetriopadre@gmail.com>
2024-03-20 01:18:21 +0100

    finally bonus 1

--- resources/js/app.js
+++
@@ -1,5 +0,0 @@
-import "~resources/scss/app.scss";
-
-import.meta.glob(["../images/**"]);
-
-import * as bootstrap from "bootstrap";
--- resources/views/partials/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="bg-dark text-white">
-    <div class="container py-4 ">
-        &copy; {{ env('APP_AUTHOR', 'Demetrio Padre') }} {{ now()->year }}
-    </div>
-
-</footer>
